f6e86ce08f92784ddecff38e853c01f544f66e0f
CCroteau28 <ccroteau28@gmail.com>
2021-03-01 22:44:31 -0700

    commit tree 1eb16ebcc1bc7178992928baa7de42b4c74b0e83

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+    if array.length == 1
+       return array.join
+    elsif array.length == 2
+       return array.join(" and ")
+    else array.length >= 3
+        last_word = array.last
+        array.pop
+        array << "and "
+        return array.join(", ") << "#{last_word}"
+    end
 end
